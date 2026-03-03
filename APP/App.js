99d93d2218a706e957ffcc1576143d91221ca7ab
kiiro <moraismoralesdeborafernanda3@gmail.com>
2026-03-03 15:00:45 +0000

    aula-02

--- APP/App.js
+++ APP/App.js
@@ -20,7 +20,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <Text>Susuh e Kiiro são fodas</Text>
       <StatusBar style="auto" />
     </View>
   );
