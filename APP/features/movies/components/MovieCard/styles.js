34df9158eaeb19aad999008a8b071491549e7e36
kiiro <moraismoralesdeborafernanda3@gmail.com>
2026-03-17 15:08:58 +0000

    fim da aula_04

---
+++ APP/features/movies/components/MovieCard/styles.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    container: {
+        marginBottom: 16,
+        backgroundColor: "#fff",
+        broderRadius: 8,
+        padding: 8
+    },
+
+    image: {
+        width: "100%",
+        height: 300,
+        borderRadius: 8
+    },
+    
+    title: {
+        fontSize: 18,
+        fontWeight: "bold",
+        paddingBlock: 8
+    },
+
+    year: {
+        fontSize: 16
+    }
+});
---
+++ APP/features/movies/hooks/useMovies.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";         
+import { getMovies} from "../services/movieServices";     
+
+export const useMovies = () => {
+    const [movies, setMovies] = useState([]);
+    const [loading, setLoading] = useState([true]);
+
+    const fetchMovies = async () => {
+        try {
+            const data = await getMovies();
+            setMovies(data);
+        } catch (error) {
+            console.log("Erro ao buscar filmes:", error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+ useEffect(() => {
+    fetchMovies();
+ }, []);
+
+ return {
+    movies,
+    loading,
+    refresh: fetchMovies,
+ };
+};
